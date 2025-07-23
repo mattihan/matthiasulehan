f6bf3ed289ca22c0a8285f68d79129127579314c
mattihan <mattihan@gmail.com>
2025-07-23 03:15:18 -0700

    refactor security.py

--- security.py
+++ security.py
@@ -1,39 +1,52 @@
 import os
 
 
-def is_permitted_path(func):
-    def wrapper(*args, **kwargs):
-        working_directory, file_path = args[:2]
-        absolute_path = os.path.join(working_directory, file_path)
-        if not os.path.abspath(absolute_path).startswith(
-            os.path.abspath(working_directory)
-        ):
-            raise PermissionError(
-                f'Cannot list "{file_path}" because it is outside the permitted working directory'
-            )
-        return func(*args, **kwargs)
-
-    return wrapper
+class SecurityFunctionArgs:
+    WORKING_DIRECTORY = 0
+    FILE_PATH = 1
+    ABSOLUTE_PATH = 2
 
 
-def is_dir(func):
-    def wrapper(*args, **kwargs):
-        working_directory, file_path = args[:2]
-        absolute_path = os.path.join(working_directory, file_path)
-        if not os.path.isdir(absolute_path):
-            raise PermissionError(f'"{file_path}" is not a directory')
-        return func(*args, **kwargs)
+def security_decorator_generator(
+    rule, error=lambda _: Exception("Rule condition triggered")
+):
+    def outer(func):
+        def inner(*args, **kwargs):
+            working_directory, file_path = args[:2]
+            absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+            if not rule(working_directory, file_path, absolute_path):
+                return error(working_directory, file_path, absolute_path)
+            return func(*args, **kwargs)
 
-    return wrapper
+        return inner
+
+    return outer
 
 
-def is_file(func):
-    def wrapper(*args, **kwargs):
-        working_directory, file_path = args[:2]
-        absolute_path = os.path.join(working_directory, file_path)
+exists = security_decorator_generator(
+    lambda *args: os.path.exists(args[SecurityFunctionArgs.ABSOLUTE_PATH]),
+    lambda *args: f'Error: File "{args[SecurityFunctionArgs.FILE_PATH]}" not found',
+)
+is_python_file = security_decorator_generator(
+    lambda *args: args[SecurityFunctionArgs.FILE_PATH][::-1][:3][::-1] == ".py",
+    lambda *args: f'Error: "{args[1]} is not a Python file',
+)
 
-        if not os.path.isfile(absolute_path):
-            raise PermissionError(f'"{file_path}" is not a file')
-        return func(*args, **kwargs)
 
-    return wrapper
+def is_permitted_path(action="list"):
+    return security_decorator_generator(
+        lambda *args: args[SecurityFunctionArgs.ABSOLUTE_PATH].startswith(
+            os.path.abspath(args[SecurityFunctionArgs.WORKING_DIRECTORY])
+        ),
+        lambda *args: f'Error: Cannot {action} "{args[SecurityFunctionArgs.FILE_PATH]}" as it is outside the permitted working directory',
+    )
+
+
+is_dir = security_decorator_generator(
+    lambda *args: os.path.isdir(args[SecurityFunctionArgs.ABSOLUTE_PATH]),
+    lambda *args: f'Error: "{args[SecurityFunctionArgs.FILE_PATH]}" is not a directory',
+)
+is_file = security_decorator_generator(
+    lambda *args: os.path.isfile(args[SecurityFunctionArgs.ABSOLUTE_PATH]),
+    lambda *args: f'Error: "{args[SecurityFunctionArgs.FILE_PATH]}" is not a file',
+)
